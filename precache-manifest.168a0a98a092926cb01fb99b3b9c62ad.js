fa36894cd924b59569edb5f62ec1a920d1931623
anci-flower <2289342907@qq.com>
2019-06-11 23:00:06 +0800

    Updates

---
+++ precache-manifest.168a0a98a092926cb01fb99b3b9c62ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "57603dfec29be159115a",
+    "url": "/medicine-app/static/js/runtime~main.41cf1577.js"
+  },
+  {
+    "revision": "b4005347182a070ce8db",
+    "url": "/medicine-app/static/js/main.f739298b.chunk.js"
+  },
+  {
+    "revision": "6a884c94ab8a9e2fd9fe",
+    "url": "/medicine-app/static/js/2.41f7792e.chunk.js"
+  },
+  {
+    "revision": "b4005347182a070ce8db",
+    "url": "/medicine-app/static/css/main.f6a5db7e.chunk.css"
+  },
+  {
+    "revision": "6a884c94ab8a9e2fd9fe",
+    "url": "/medicine-app/static/css/2.cae9e12a.chunk.css"
+  },
+  {
+    "revision": "2d45f455bb026fbdbd1afe40818366bb",
+    "url": "/medicine-app/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/medicine-app/precache-manifest.168a0a98a092926cb01fb99b3b9c62ad.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/medicine-app/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
